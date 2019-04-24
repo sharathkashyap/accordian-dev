37e385231387026353d9d88bc9f8199843dee420
ankur01oct <ankur01oct@gmail.com>
2019-04-24 17:51:34 +0530

    Issue SB-11938 feat: FAQ UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,4 +10,22 @@ $(document).ready(function(){
         }).on('hide.bs.collapse', function(){
         	$(this).parent().find(".fa-chevron-down").removeClass("rotate");
         });
+    
+        //Add Event Listener on click of Yes and No button 
+        $('[id="btn-yes"]').click(function(){
+           console.log("Yes-Clicked");
+           console.log("Question :" , ($(this).parent().parent().parent().find("h4").first().text().trim()) );
+           console.log("Answer :" , ($(this).parent().parent().find("p").first().text()) );
+//                   console.log($(this).parent().children().splice(-1,1))
+           $(this).parent().find('button').hide();
+           $(this).parent().find("p").first().hide();
+            $(this).parent().children().last().removeAttr('hidden')
+
+        })
+    
+        $('[id="btn-no"]').click(function(){
+           console.log("No-Clicked");
+           console.log("Question :" , ($(this).parent().parent().parent().find("h4").first().text().trim()) );
+           console.log("Answer :" , ($(this).parent().parent().find("p").first().text()) );
+        })
     });
